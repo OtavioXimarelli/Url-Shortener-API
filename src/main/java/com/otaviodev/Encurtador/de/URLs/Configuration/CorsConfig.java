201da6884a17eb80cde85841cea19c60bee932dd
OtavioXimarelli <ximarellio@gmail.com>
2025-03-03 14:12:48 -0300

    fix cors again

--- src/main/java/com/otaviodev/Encurtador/de/URLs/Configuration/CorsConfig.java
+++ src/main/java/com/otaviodev/Encurtador/de/URLs/Configuration/CorsConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.beans.BeanProperty;
+
 @Configuration
 public class CorsConfig {
+    @BeanProperty
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://encurtador-de-url-dun.vercel.app/")
