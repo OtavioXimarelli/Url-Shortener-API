09bfc8f841e6bbd3f5d5d1bebc5bdf383525e228
OtavioXimarelli <ximarellio@gmail.com>
2025-03-03 17:46:15 -0300

    fix a sytax error in file

--- src/main/java/com/otaviodev/Encurtador/de/URLs/Configuration/CorsConfig.java
+++ src/main/java/com/otaviodev/Encurtador/de/URLs/Configuration/CorsConfig.java
@@ -1,27 +1,24 @@
 package com.otaviodev.Encurtador.de.URLs.Configuration;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.beans.BeanProperty;
-
 @Configuration
 public class CorsConfig {
-    @BeanProperty
+
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://encurtador-de-url-dun.vercel.app/")
-                        .allowedMethods("GET", "OPTIONS", "POST")
+                        .allowedOrigins("https://encurtador-de-url-dun.vercel.app")
+                        .allowedMethods("GET", "OPTIONS", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
-
-
 }
