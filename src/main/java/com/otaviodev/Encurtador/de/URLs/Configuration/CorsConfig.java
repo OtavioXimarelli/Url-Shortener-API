8dccac49d2d436a18666768fea6fab4fb273f4fe
OtavioXimarelli <ximarellio@gmail.com>
2025-03-03 13:58:59 -0300

    add cors config for aws, for now, the origins is all ips for test

---
+++ src/main/java/com/otaviodev/Encurtador/de/URLs/Configuration/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.otaviodev.Encurtador.de.URLs.Configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET",  "POST")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+
+}
